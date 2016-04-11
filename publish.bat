3823c63ca81e7f06bebbb5ab231668e255b256ec
Richard <richardk@leapbeyond.com>
2016-04-11 07:34:12 -0400

    Test commit

--- publish.bat
+++ publish.bat
@@ -1,6 +1,24 @@
 @echo off
+
+CheckIfCurrent.sh ./ https://github.com/rkagerer/KSP-Fwiffo-Repository
+if errorlevel 1 goto NOTCURRENT
+
+git commit -a
+git push origin master
+
 del CKAN-meta-fwiffo.zip
 REM -tzip for zip, -tgzip for gzip, -ttar for tar
 "C:\Program Files (x86)\7-Zip\7z.exe" a -tzip -r -x@CKAN-meta-fwiffo\exclude.txt CKAN-meta-fwiffo.zip CKAN-meta-fwiffo\*.*
 echo Remember to upload to GITHUB.
+goto END
+
+:NOTCURRENT
+echo.
+echo ************************ FAILED! *************************
+echo Local copy is out of date.  Use GIT to pull changes first.
+echo e.g. git pull origin master [--rebase]
+echo **********************************************************
+echo.
+
+:END
 pause
